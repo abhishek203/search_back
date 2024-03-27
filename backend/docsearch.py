75f48f1441e2aa7d99fcfb9970545568da24ea68
Abhishek E <abhisheke@enphaseenergy.com>
2024-03-27 21:56:52 +0530

    first commit

--- backend/docsearch.py
+++ backend/docsearch.py
@@ -1,42 +1,46 @@
 from fastapi import FastAPI, HTTPException
-import os
-import dotenv
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
 from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from fastapi.middleware.cors import CORSMiddleware
+import time
 
-dotenv.load_dotenv()
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 app = FastAPI()
+origins = [
+    "https://chat.openai.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 @app.post("/create-vector-db/")
 async def create_vector_db():
-    try:
-        loader = DirectoryLoader("./data")
-        documents = loader.load()
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-        docs = text_splitter.split_documents(documents)
-        embeddings = OpenAIEmbeddings()
-        db = FAISS.from_documents(docs, embeddings)
-        db.save_local("faiss_index")
-        return {"message": "Vector database created successfully."}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
+    
+    loader = DirectoryLoader("./data")
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = text_splitter.split_documents(documents)
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    db = FAISS.from_documents(docs, embeddings)
+    db.save_local("faiss_index")
+    return {"message": "Vector database created successfully."}
+    
 @app.get("/inference/")
 async def inference(query: str):
-    try:
-        embeddings = OpenAIEmbeddings()
-        new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
-        docs_and_scores = new_db.similarity_search_with_score(query)
-        if docs_and_scores[0][1] < 0.5:
-            return {"match": True, "score": str(docs_and_scores[0][1])}
-        return {"match": False, "score": str(docs_and_scores[0][1])}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs_and_scores = new_db.similarity_search_with_score(query)
+    if docs_and_scores[0][1] < 1:
+        return {"response": True, "score": str(docs_and_scores[0][1])}
+    return {"response": False, "score": str(docs_and_scores[0][1])}
+    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
